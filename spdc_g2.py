6d55b83ea89baac64a08ec09ed0e2204d704df5e
JamArm99 <jaarmitage1@sheffield.ac.uk>
2021-07-23 14:24:50 +0100

    Editing Data Analysis

--- spdc_g2.py
+++ spdc_g2.py
@@ -3,8 +3,10 @@
 #Importing python libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.function_base import angle
 import scipy.optimize as spy
 import os
+import csv
 
 #Uncomment the next two lines if the input .txt files are large.
 #import sys
@@ -12,82 +14,146 @@ import os
 
 import func
 
-#Defining plot params
+#Defining plot and file params
 fnt = 20
 wht = 'bold'
+fieldnames = ['Parameter', 'Value', 'Error']
 
 #Creating folder for output csv files
 if os.path.isdir('spdc_g2_csv_files') == True:
     pass#If folder already exists then do nothing to prevent os error
 else:
-     os.os.mkdir('spdc_g2_csv_files')
+    print('Creating folder named spdc_g2_csv_files for data output')
+    os.mkdir('spdc_g2_csv_files')
+
+#Creating folder for ouput png files
+if os.path.isdir('spdc_g2_png_files') == True:
+    pass
+else:
+    print('Creating folder named spdc_g2_png_files for image output')
+    os.mkdir('spdc_g2_png_files')
 
 #Angular Dependence analysis
 
 #Data collected in the quantum lab
-X = [-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1]#millimetres
-x = 10*np.array(X)#centimetres
-coincidence = [15,25,350,3000,5700,5800,4300,700,50,20,20]
+x_cm = [-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1]#cm
+x_mm = 10*np.array(x_cm)#mm conversion
+coincidence_angle = [15,25,350,3000,5700,5800,4300,700,50,20,20]
 
 #Uncomment the next lines if the file is to be loaded in via a .txt file
 #x, coincidence = np.loadtxt('name of file', unpack = 'True', delimiter = 'delimiter in .txt file')
 
 #xdata and inital params for scipy
-xdata = np.linspace(x[0],x[-1], 1000)
-amp = 5800
-x0 = 0
-sd = 0.03*(2*x[-1])
+xdata_angle = np.linspace(x_mm[0],x_mm[-1], 1000)#xdata to pass through scipy function
+amp_angle = 5800
+x0_angle = 0
+sd_angle = 0.03*(2*x_mm[-1])
 
-p = [amp,x0,sd]
-popt, pcov  = spy.curve_fit(func.gaussian, x, coincidence, p)
-ydata = func.gaussian(xdata,popt[0],popt[1],popt[2])
+p_angle = [amp_angle,x0_angle,sd_angle]#Initial guess list for scipy optimise
+#Optimised parameters and covarience matrix for angluar dependence
+popt_angle, pcov_angle  = spy.curve_fit(func.gaussian, x_mm, coincidence_angle, p_angle)
+
+angle_dic = {}
+
+#Gaussian parameters for angular dependence
+for i in range(0,3):
+    if i == 0:
+        peak_height_angle = popt_angle[i]#Optimised amplitude
+        peak_error_angle = np.sqrt(pcov_angle[i,i])#Error on amplitude
+        angle_dic['Peak'] = peak_height_angle,peak_error_angle
+    elif i == 1:
+        peak_centre_angle = popt_angle[1]#Optimised x0
+        centre_error_angle = np.sqrt(pcov_angle[i,i])#Error on peak centre
+        angle_dic['Centre'] = [peak_centre_angle, centre_error_angle]
+    else:
+        sigma_angle = popt_angle[2]#Optimised Sigma
+        sigma_error_angle = np.sqrt(pcov_angle[i,i])#Error on sigma
+        angle_dic['Sigma'] = sigma_angle,sigma_error_angle
+
+#Full width at half maximum
+fwhm_angle = func.FWHM(sigma_angle)
+fwhm_error_angle = np.sqrt(pcov_angle[2,2])/sigma_angle * fwhm_angle
+angle_dic['FWHM'] = [fwhm_angle,fwhm_error_angle]
+
+#Writing file to csv folder
+with open('spdc_g2_csv_files/angle_dependence.csv', 'w') as file_angle:
+    w = csv.writer(file_angle, delimiter = ',')
+    w.writerow(fieldnames)
+    for key in angle_dic:
+        row = [key] + [angle_dic[key][0]] + [angle_dic[key][1]]
+        w.writerow(row)
+
+#Gaussian model data
+ydata_angle = func.gaussian(xdata_angle,peak_height_angle,peak_centre_angle,sigma_angle)
 
 #matplotlib plot for angular dependence
 fig = plt.figure(figsize = (10,8))
-plt.plot(x,coincidence, 'o-', label = 'Data',color = 'black')
-plt.plot(xdata,ydata, label = 'Gaussian Model', color = 'Red')
+plt.plot(x_mm,coincidence_angle, 'o-', label = 'Data',color = 'black')#Data plot
+plt.plot(xdata_angle,ydata_angle, label = 'Gaussian Model', color = 'Red')#Gaussian Model
 plt.xlabel(' Displacement [mm]', fontsize = fnt, fontweight = wht)
 plt.ylabel('Coincidence Rate', fontsize = fnt, fontweight = wht)
 plt.minorticks_on()
 plt.ylim(0,7000)
 plt.tick_params(labelsize = fnt)
 plt.legend(fontsize = fnt)
-plt.savefig('Angular_Dependence.png')
-plt.show()
+plt.savefig('spdc_g2_png_files/Angular_Dependence.png')
+#plt.show()
 
-#Gaussian analysis printed to terminal
-fwhm = func.FWHM(popt[2])
-error = np.sqrt(pcov[2, 2]) / popt[2] * fwhm
-print('*********************************************')
-print('Angular dependence analysis:')
-print('FWHM  =  %.1f +/- %.1f' % (fwhm, error))
-print('Peak  =  %.f +/- %.f ' % ( popt[0], np.sqrt(pcov[0, 0])))
-print('Peak Centre = %.1f +/- %.2f' % (popt[1], np.sqrt(pcov[1, 1])))
-print('Sigma =  %.2f +/- %.2f ' % (popt[2], np.sqrt(pcov[2, 2])))
-
-#Polarisation Dependence analysis (vars overwritten)
+#Polarisation Dependence analysis
 
 #Data collected in the quantum lab
-polar = [-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80,90]
-coincidence2 = [0,0,5,25,130,470,1160,2210,3200,3600,3400,2660,1624,699,239,48,4,0,0]
+polar = [-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80,90]#Degrees
+coincidence_polar = [0,0,5,25,130,470,1160,2210,3200,3600,3400,2660,1624,699,239,48,4,0,0]
 
 #Uncomment the next lines if the file is to be loaded in via a .txt file
 #polar, coincidence2 = np.loadtxt('name of file', unpack = 'True', delimiter = 'delimiter in .txt file')
 
 #xdata and inital params for scipy
-xdata = np.linspace(polar[0],polar[-1], 1000)
-amp = 3600
-x0 = 0
-sd = 0.03*(2*polar[-1])
+xdata_polar = np.linspace(polar[0],polar[-1], 1000)
+amp_polar = 3600
+x0_polar = 0
+sd_polar = 0.03*(2*polar[-1])
 
-p = [amp,x0,sd]
-popt, pcov  = spy.curve_fit(func.gaussian, polar, coincidence2, p)
-ydata = func.gaussian(xdata,popt[0],popt[1],popt[2])
+p_polar = [amp_polar,x0_polar,sd_polar]#Initial guess list for scipy optimise
+popt_polar, pcov_polar  = spy.curve_fit(func.gaussian, polar, coincidence_polar, p_polar)
+
+polar_dic = {}
+
+#Gaussian parameters for angular dependence
+for i in range(0,3):
+    if i == 0:
+        peak_height_polar = popt_polar[i]#Optimised amplitude
+        peak_error_polar = np.sqrt(pcov_polar[i,i])#Error on amplitude
+        polar_dic['Peak'] = peak_height_polar,peak_error_polar
+    elif i == 1:
+        peak_centre_polar = popt_polar[1]#Optimised x0
+        centre_error_polar = np.sqrt(pcov_polar[i,i])#Error on peak centre
+        polar_dic['Centre'] = [peak_centre_polar, centre_error_polar]
+    else:
+        sigma_polar = popt_polar[2]#Optimised Sigma
+        sigma_error_polar = np.sqrt(pcov_polar[i,i])#Error on sigma
+        polar_dic['Sigma'] = sigma_polar,sigma_error_polar
+
+#Full width at half maximum
+fwhm_polar = func.FWHM(sigma_polar)
+fwhm_error_polar = np.sqrt(pcov_polar[2,2])/sigma_polar * fwhm_polar
+polar_dic['FWHM'] = [fwhm_polar,fwhm_error_polar]
+
+#Writing file to csv folder
+with open('spdc_g2_csv_files/polar_dependence.csv', 'w') as file_polar:
+    w = csv.writer(file_polar, delimiter = ',')
+    w.writerow(fieldnames)
+    for key in angle_dic:
+        row = [key] + [polar_dic[key][0]] + [polar_dic[key][1]]
+        w.writerow(row)
+
+#Gaussian model data
+ydata_polar = func.gaussian(xdata_polar,popt_polar[0],popt_polar[1],popt_polar[2])
 
 #matplotlib plot for polarisation dependence
 fig = plt.figure(figsize = (10,8))
-plt.plot(polar,coincidence2, 'o-', label = 'Data',color = 'black')
-plt.plot(xdata,ydata, label = 'Gaussian Model', color = 'Red')
+plt.plot(polar,coincidence_polar, 'o-', label = 'Data',color = 'black')#Data
+plt.plot(xdata_polar,ydata_polar, label = 'Gaussian Model', color = 'Red')#Gaussian Model
 plt.xlabel(r'$\Delta\Theta$ [$^\circ$]', fontsize = fnt, fontweight = wht)
 plt.ylabel('Coincidence Rate', fontsize = fnt, fontweight = wht)
 plt.minorticks_on()
@@ -96,22 +162,12 @@ plt.legend(fontsize = fnt)
 plt.xlim(-90,90)
 plt.ylim(0,4000)
 plt.savefig('Polarisation_Dependence.png')
-plt.show()
+#plt.show()
 
-#Gaussian analysis printed to terminal
-fwhm = func.FWHM(popt[2])
-error = np.sqrt(pcov[2, 2]) / popt[2] * fwhm
-print('*********************************************')
-print('Polarisation dependence analysis:')
-print('FWHM  =  %.2f +/- %.2f' % (fwhm, error))
-print('Peak  =  %.f +/- %.f ' % ( popt[0], np.sqrt(pcov[0, 0])))
-print('Peak Centre = %.1f +/- %.2f' % (popt[1], np.sqrt(pcov[1, 1])))
-print('Sigma =  %.2f +/- %.2f ' % (popt[2], np.sqrt(pcov[2, 2])))
-
-#Laser Emission Spectrum (vars overwritten)
+#Laser Emission Spectrum
 
 #Loading data from .txt files
-wavelength, intensity = np.loadtxt('Emission.txt', unpack = 'True')
+wavelength, intensity = np.loadtxt('txt_input_files/Emission.txt', unpack = 'True')
 
 #xdata and inital params for scipy
 xdata = np.linspace(770,830, 1000)
