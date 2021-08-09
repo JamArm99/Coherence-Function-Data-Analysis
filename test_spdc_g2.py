5d9724f03bb59ba9f972c43d25ba36c9946193d5
JamArm99 <jaarmitage1@sheffield.ac.uk>
2021-08-09 13:57:56 +0100

    Update test_spdc_g2.py

--- test_spdc_g2.py
+++ test_spdc_g2.py
@@ -1,5 +1,5 @@
 import unittest
-import func
+import func_spdc_g2
 import numpy as np
 from scipy.stats import shapiro
 
@@ -8,18 +8,18 @@ class TestSPDC(unittest.TestCase):
     #Test Gaussian function
     def test_gaus(self):
         xdata = np.linspace(-1,1,100)#Create some xdata for gaussian
-        stat, p = shapiro(func.gaussian(xdata,1,0,0.1))#Statistical power 
+        stat, p = shapiro(func_spdc_g2.gaussian(xdata,1,0,0.1))#Statistical power 
 
         self.assertLessEqual(p, 0.05)#Shapiro-Wilk test for Gaussian distribution
         
         with self.assertRaises(ValueError):
-            func.gaussian(xdata,0,0,1)#0 coefficient is not allowed
-            func.gaussian(xdata,1,0,0)#0 sigma is not allowed
-            func.gaussian(xdata,0,0,0)#Both sigma and coefficient
+            func_spdc_g2.gaussian(xdata,0,0,1)#0 coefficient is not allowed
+            func_spdc_g2.gaussian(xdata,1,0,0)#0 sigma is not allowed
+            func_spdc_g2.gaussian(xdata,0,0,0)#Both sigma and coefficient
 
     def test_fwhm(self):
         with self.assertRaises(ValueError):
-            func.FWHM(0)#0 sigma
+            func_spdc_g2.FWHM(0)#0 sigma
         
 
 #Running unittest directly in terminal/editor
